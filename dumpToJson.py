20b3d9e75416d8ee5b16e27e0711a5dc6c9f4c00
Artefact2 <artefact2@gmail.com>
2012-07-29 17:32:04 +0200

    Add a command-line switch to only dump certain tables.

--- dumpToJson.py
+++ dumpToJson.py
@@ -1,6 +1,7 @@
 """Cache dumper script, uses phobos to dump a json dump to disk
 
 Copyright (c) 2012 Diego "Sakari" Duclos <sakari@evefit.org>
+Copyright (c) 2012 Romain "Artefact2" Dalmaso <artefact2@gmail.com>
 
 This code is free software; you can redistribute it and/or modify
 it under the terms of the BSD license (see the file LICENSE.txt
@@ -14,6 +15,11 @@ from reverence import blue
 from phobos.writer.jsonWriter import JsonWriter
 from phobos.remoteSvc import discover as discoverSvc
 from phobos.rowSetProcessor import RowSetProcessor
+
+def processRowSet(tableName, rowSet, output, indent):
+	print("processing {}".format(tableName))
+	header, lines = RowSetProcessor(tableName, rowSet).run()
+	JsonWriter(tableName, header, lines, output, indent).run()
 
 if __name__ == "__main__":
 	try:
@@ -33,34 +39,44 @@ if __name__ == "__main__":
 	parser.add_argument("-s", "--server", default='tranquility', help="If we're dealing with a singularity cache")
 	parser.add_argument("-o", "--output", help="Output folder for the json files", required=True)
 	parser.add_argument("-i", "--indent", action="store_true", help="Use pretty indentation for json files", default=False)
+	parser.add_argument("-t", "--tables", help="comma-separated list of table names to dump (all tables are dumped by default)")
 	args = parser.parse_args()
 
 	# Initialize reverence object, get tables and write them out to json
 	eve = blue.EVE(args.eve, cachepath=args.cache, server=args.server)
 	cfg = eve.getconfigmgr()
+	indent = 4 if args.indent else None
+	output = args.output
 
-	# Process bulkdata tables
-	for tableName in cfg.tables:
-		print("processing {}".format(tableName))
-		try:
-			rowSet = getattr(cfg, tableName)
-			header, lines = RowSetProcessor(tableName, rowSet).run()
-		except:
-			print("failed to process {}".format(tableName))
-		else:
-			JsonWriter(tableName, header, lines, args.output).run()
+	# If -t is present, only dump the specified tables
+	if(args.tables != None):
+		for tableName in args.tables.split(","):
+			try:
+				t = tableName.split("_", 2)
+				if(len(t) == 2):
+					rowSet = getattr(eve.RemoveSvc(t[0]), t[1])()
+				else:
+					rowSet = getattr(cfg, t[0])
+				processRowSet(tableName, rowSet, output, indent)
+			except:
+				print("failed to process {}".format(tableName))
+	else:
+		# Process bulkdata tables
+		for tableName in cfg.tables:
+			try:
+				rowSet = getattr(cfg, tableName)
+				processRowSet(tableName, rowSet, output, indent)
+			except:
+				print("failed to process {}".format(tableName))
 
-	# Process remote service calls
-	for service, call in discoverSvc(eve):
-		tableName = "{}_{}".format(service, call)
-		print("processing {}".format(tableName))
-		try:
-			rowSet = getattr(eve.RemoteSvc(service), call)()
-			header, lines = RowSetProcessor(tableName, rowSet).run()
-		except:
-			print("Failed to process {}".format(tableName))
-		else:
-			JsonWriter(tableName, header, lines, args.output, indent=4 if args.indent else None).run()
+		# Process remote service calls
+		for service, call in discoverSvc(eve):
+			tableName = "{}_{}".format(service, call)
+			try:
+				rowSet = getattr(eve.RemoveSvc(service), call)()
+				processRowSet(tableName, rowSet, output, indent)
+			except:
+				print("failed to process {}".format(tableName))
 
 	"""
 	#Debug code
