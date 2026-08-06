09b05d92d0f4d14176240be188bac4186ca611b0
DarkPhoenix <phoenix@mail.ru>
2026-08-06 23:59:36 +0200

    Minor style fixes

--- miner/fsd_binary/fsd/decoder.py
+++ miner/fsd_binary/fsd/decoder.py
@@ -120,8 +120,7 @@ def load_union(data, offset, schema, path):
     type_index = u32(data, offset, path)
     options = schema.get('optionTypes', ())
     if type_index >= len(options):
-        raise FsdFormatError('union option {} is outside {} choices at {}'.format(
-            type_index, len(options), path))
+        raise FsdFormatError('union option {} is outside {} choices at {}'.format(type_index, len(options), path))
     option = options[type_index]
     return decode(data, offset + U32.size, option, path.child('<{}>'.format(option.get('type'))))
 
@@ -158,8 +157,7 @@ def load_object(data, offset, schema, path):
         if name in fixed_offsets:
             result[name] = decode(data, offset + fixed_offsets[name], attribute_schema, child_path)
         elif name in variable_offsets:
-            result[name] = decode(
-                data, variable_base + variable_offsets[name], attribute_schema, child_path)
+            result[name] = decode(data, variable_base + variable_offsets[name], attribute_schema, child_path)
         elif 'default' in attribute_schema:
             result[name] = attribute_schema['default']
         elif 'isOptional' not in attribute_schema:
@@ -228,8 +226,7 @@ def load_dict(data, offset, schema, path):
 
 def load_index(data, offset_to_data, schema, path, offset_to_footer=0):
     object_size = u32(data, offset_to_data, path)
-    footer_size_offset = (offset_to_footer - U32.size if offset_to_footer
-                          else offset_to_data + object_size)
+    footer_size_offset = (offset_to_footer - U32.size if offset_to_footer else offset_to_data + object_size)
     if footer_size_offset < 0 or footer_size_offset + U32.size > len(data):
         raise FsdFormatError('index footer size offset {} is outside {} at {}'.format(footer_size_offset, len(data), path))
     footer_size = u32(data, footer_size_offset, path)
